c19ba42f9964ba034cb6574a40c095847ecd6bf0
Lukeuke <lukasz.mrowiec10102004@gmail.com>
2022-11-22 18:53:30 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KornikTournament.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
